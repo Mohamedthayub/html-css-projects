72297c5f403492a4a0968755d914e36abdbedc22
buyath <buyaththayub@gmail.com>
2025-04-25 17:50:15 +0000

    readme added in newsletter sign in

--- newsletter-sign-up-with-success-message-main (1)/newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main (1)/newsletter-sign-up-with-success-message-main/index.js
@@ -19,7 +19,7 @@ user_btn.addEventListener("click", function (e) {
         user_error.style.display = "none";
     }
     userinput.textContent = "";
-    window.location.href = "http://127.0.0.1:5500/html_css_projects/newsletter-sign-up-with-success-message-main%20(1)/newsletter-sign-up-with-success-message-main/index2.html"
+    window.location.href = "http://127.0.0.1:5500/html_css_projects/newsletter-sign-up-with-success-message-main%20(1)/newsletter-sign-up-with-success-message-main/pages/thankletter.html";
         
 
 });
